da43ca8c79277d863df2a2d6198e2d2bef3e31e3
Anton Pomozov <anton.pomozov@ooma.com>
2018-08-10 16:44:11 +0300

    Initial Commit

---
+++ StackViewAnimation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StackViewAnimation
+//
+//  Created by Anton Pomozov on 10/08/2018.
+//  Copyright © 2018 Ooma Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
