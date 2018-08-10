38991034bb846a3341e8011ecfc6c60c9908c030
Anton Pomozov <anton.pomozov@ooma.com>
2018-08-10 18:14:53 +0300

    The solution without stack view

--- StackViewAnimation/ViewController.swift
+++ StackViewAnimation/ViewController.swift
@@ -10,19 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var stack: UIStackView!
     @IBOutlet var buttons: [UIButton]!
     @IBOutlet var zeroHeightConstraint: NSLayoutConstraint!
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-//        buttons.forEach { $0.isHidden = true }
-    }
-
     @IBAction func action(_ sender: UIButton) {
         zeroHeightConstraint.isActive = !zeroHeightConstraint.isActive
-//        buttons.forEach { $0.isHidden = !$0.isHidden }
 
         UIView.animate(withDuration: 1.0,
                        delay: 0.5,
@@ -34,4 +26,3 @@ class ViewController: UIViewController {
     }
 
 }
-
